7cc7bf826df729e64eb95b054efadf6f88344ba8
Dwei99 <alexanderanderson1@gmail.com>
2017-01-31 23:45:40 -0600

    initial commit for discussion threead

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,12 +12,35 @@ class ItemsController < ApplicationController
   def show_item
     @item = Item.find_by_id(params[:id])
     @categories = Category.all
+    @discussion = Discussion.find_by_item_id (@item.id)
     @cart = Cart.find_by_id(session[:cart_id])
     if @cart.nil?
       @cart = Cart.create
       session[:cart_id] = @cart.id
     end
     render 'show_item'
+  end
+
+  def show_discussion
+    @item = Item.find_by_id(params[:id])
+    @discussions = Item.find(@item).discussions
+    @categories = Category.all
+    render 'show_discussion'
+  end
+
+  def create_post
+    if !session[:id].present?
+      flash[:notice] = "If you want to post a comment, please log in."
+      return redirect_to "/items/discussion/#{item.id}"
+    end
+    user = User.find_by_id(session[:id])
+    item = Item.find_by_id(params[:item_id])
+    disscussion_post = Discussion.create(content:params[:content],item:item,user:user)
+    if !disscussion_post.errors.blank?
+      flash[:errors] = disscussion_post.errors.messages
+      return redirect_to "/items/discussion/#{item.id}"
+    end
+    return redirect_to "/items/discussion/#{item.id}"
   end
 
   def checkout
@@ -28,10 +51,9 @@ class ItemsController < ApplicationController
     cart = Cart.find(session[:cart_id])
     item = Item.find(params[:id])
     CartItem.create(cart: cart, item: item)
-<<<<<<< HEAD
-=======
     id = item.id
->>>>>>> 11861de46a727581af46a7ccf62e3e8b080b3cfe
     redirect_to "/items/show_item/#{item.id}"
   end
+
+
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,6 @@
 class Discussion < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+
+  validates :content, :presence => {message:["content cant be blank"]}
 end
